883895402d2eb927a0addfdafe2260f6252f40c4
Mihail Eric <mihail911@gmail.com>
2015-01-02 00:38:28 -0800

    started working on features and creating a train suite

--- features/features.py
+++ features/features.py
@@ -7,6 +7,7 @@ sys.path.append(root_dir)
 
 from util.DataStreamer import DataStreamer
 from sklearn.base import BaseEstimator, TransformerMixin
+import numpy as np
 
 
 class IdentityFeatures(BaseEstimator, TransformerMixin):
@@ -15,11 +16,21 @@ class IdentityFeatures(BaseEstimator, TransformerMixin):
     def __init__(self):
         pass
 
+    def get_feature_names(self):
+        return np.array(['ad_width', 'ad_height']) #try silly features for proof-of-concept
+
     def fit(self, data_points, y=None):
         pass
 
     def transform(self, data_points):
-        pass
+        features = []
+        #widths = [d.data['C15'] for d in data_points]
+        #heights = [d.data['C16'] for d in data_points]
+        for d in data_points:
+            features.append([d.data['C15', d.data['C16']]])
+
+        return np.array(features)
 
     def fit_transform(self, data_points, y=None, **fit_params):
+        #May implement this later if necessary
         pass
