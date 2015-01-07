5ea9b6eb94e6fd827e33f2df5dd8d2201325026d
Mihail Eric <mihail911@gmail.com>
2015-01-07 01:26:51 -0800

    wrote a basic log reg model and started writing training code

--- models/models.py
+++ models/models.py
@@ -6,10 +6,23 @@ root_dir = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(root_dir)
 
 from sklearn.pipeline import Pipeline
-from features.features import IdentityFeatures
+from features.features import IdentityFeatures, IPFeatures, FeatureStacker
+from sklearn.linear_model import LogisticRegression
 
 
-def build_naive_bayes_model(*args):
-    """Builds a basic naive bayes model using certain feature set.
-    Returns as a pipeline with specified features."""
+def build_logistic_regression_model(*args):
+    """Builds a logistic regression model using certain feature set, specified
+    via 'args'. Returns as a pipeline with specified features."""
+    model = LogisticRegression() #TODO: tune hyperparameters for model
+
+    ###Maybe apply other data transforms (e.g. TF-IDF, K-best, PCA, etc.)
+
+    features = FeatureStacker([feat for feat in args]) #make list of features to be applied
+    pipeline = Pipeline([('feat', features), ('log_reg', model)])
+    return pipeline
+
+#TODO: Make an SVM model
+
+
+
 
